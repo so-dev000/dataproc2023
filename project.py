95ae4c67a6fb8c75d545ecd4a23797f16e7553ce
nsota-akademia <yuasoi.so@gmail.com>
2023-12-23 18:46:29 +0900

    飲食店プロット

--- project.py
+++ project.py
@@ -10,7 +10,7 @@ from requests import Response
 load_dotenv()
 MAP_BOX_ACCESS_TOKEN: Final = os.getenv("MAP_BOX_ACCESS_TOKEN")
 HOT_PEPPER_API_KEY: Final = os.getenv("HOT_PEPPER_API_KEY")
-API_URL: Final = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+HOT_PEPPER_API_URL: Final = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
 
 
 def fetch_data(keyword: str) -> dict:
@@ -20,7 +20,7 @@ def fetch_data(keyword: str) -> dict:
         "format": "json",
         "count": 100,
     }
-    request: Response = requests.get(API_URL, params=params)
+    request: Response = requests.get(HOT_PEPPER_API_URL, params=params)
     result: dict = json.loads(request.content)["results"]
     results_available: int = result["results_available"]
     results_start: int = result["results_start"]
@@ -28,43 +28,45 @@ def fetch_data(keyword: str) -> dict:
 
     start: int = results_start
     while start <= results_available:
-        print(str(results_start) + "/" + str(results_available))
+        print("\r" + str(results_start) + "/" + str(results_available), end="")
         start += 100
         params["start"] = start
-        request: Response = requests.get(API_URL, params=params)
+        request: Response = requests.get(HOT_PEPPER_API_URL, params=params)
         result: dict = json.loads(request.content)["results"]
         results_start: int = result["results_start"]
         shop += result["shop"]
-    print("done")
+    print("\r" + str(results_available), end="")
     return shop
 
 
-data: dict = fetch_data("お好み焼き")
+def show_figure(lat: list[int], lon: list[int]):
+    figure = go.Figure(
+        go.Scattermapbox(
+            lat=lat,
+            lon=lon,
+            mode="markers",
+            marker=go.scattermapbox.Marker(size=5),
+        )
+    )
+
+    figure.update_layout(
+        autosize=True,
+        hovermode="closest",
+        mapbox=dict(
+            accesstoken=MAP_BOX_ACCESS_TOKEN,
+            center=dict(lat=38.5, lon=137.5),
+            zoom=5,
+            pitch=0,
+            bearing=0,
+        ),
+    )
+    figure.show()
+
+
+data: dict = fetch_data("パエリア")
 lat: list[int] = []
-lng = []
+lon: list[int] = []
 for i in data:
     lat.append(i["lat"])
-    lng.append(i["lng"])
-
-
-figure = go.Figure(
-    go.Scattermapbox(
-        lat=lat,
-        lon=lng,
-        mode="markers",
-        marker=go.scattermapbox.Marker(size=9),
-    )
-)
-
-figure.update_layout(
-    autosize=True,
-    hovermode="closest",
-    mapbox=dict(
-        accesstoken=MAP_BOX_ACCESS_TOKEN,
-        center=dict(lat=38.5, lon=137.5),
-        zoom=3,
-        pitch=0,
-        bearing=0,
-    ),
-)
-figure.show()
+    lon.append(i["lng"])
+show_figure(lat, lon)
